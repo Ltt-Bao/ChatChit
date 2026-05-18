57f268f5c680f8ae8822cb61b2a68292be2cb7d5
KhanhTung <tung31net@gmail.com>
2026-05-19 00:48:16 +0700

    homePage test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,73 @@
-import {BrowserRouter, Route, Routes} from 'react-router';
-import SignInPage from './pages/SignInPage';
-import SignUpPage from './pages/SignUpPage';
-import ChatAppPage from './pages/ChatAppPage';
-import {Toaster} from 'sonner';
-import ProtectedRoute from './components/auth/ProtectedRoute';
-import { useThemeStore } from './stores/useThemeStore';
-import { useEffect } from 'react';
-import { useAuthStore } from './stores/useAuthStore';
-import { useSocketStore } from './stores/useSocketStore';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+import ChatAppPage from "./pages/ChatAppPage";
+import HomePage from "./pages/Homepage";
+import { Toaster } from "sonner";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import { useThemeStore } from "./stores/useThemeStore";
+import { useEffect } from "react";
+import { useAuthStore } from "./stores/useAuthStore";
+import { useSocketStore } from "./stores/useSocketStore";
 
 function App() {
-  const{isDark, setTheme} = useThemeStore();
-  const {accessToken} = useAuthStore();
-  const {connectSocket, disconnectSocket} = useSocketStore();
+  const { isDark, setTheme } = useThemeStore();
+  const { accessToken } = useAuthStore();
+  const { connectSocket, disconnectSocket } = useSocketStore();
 
   useEffect(() => {
     setTheme(isDark);
-  },[isDark]);
+  }, [isDark, setTheme]);
 
   useEffect(() => {
-    if(accessToken){
+    if (accessToken) {
       connectSocket();
     }
-    return () => disconnectSocket();
-  }, [accessToken]);
+
+    return () => {
+      disconnectSocket();
+    };
+  }, [accessToken, connectSocket, disconnectSocket]);
+
   return (
     <>
-      <Toaster richColors/>
+      <Toaster richColors />
+
       <BrowserRouter>
-        <Routes> 
+        <Routes>
           {/* public routes */}
-          <Route 
-            path='/signin'
-            element={<SignInPage/>}
+          <Route
+            path="/"
+            element={
+              accessToken ? <Navigate to="/chat" replace /> : <HomePage />
+            }
           />
 
-          <Route 
-            path='/signup'
-            element={<SignUpPage/>}
+          <Route
+            path="/signin"
+            element={
+              accessToken ? <Navigate to="/chat" replace /> : <SignInPage />
+            }
+          />
+
+          <Route
+            path="/signup"
+            element={
+              accessToken ? <Navigate to="/chat" replace /> : <SignUpPage />
+            }
           />
 
           {/* protected routes */}
-
-          <Route element={<ProtectedRoute/>}>
-            <Route 
-            path='/'
-            element={<ChatAppPage/>}
-          />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/chat" element={<ChatAppPage />} />
           </Route>
 
+          {/* fallback */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
